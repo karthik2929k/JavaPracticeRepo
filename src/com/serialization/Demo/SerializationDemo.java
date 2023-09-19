9c436fcf6a779c3ed0dd0373c5726a8a345ea723
karthik2929k <penchalakarthik9@gmail.com>
2023-09-19 16:03:51 +0530

    Test change

--- src/com/serialization/Demo/SerializationDemo.java
+++ src/com/serialization/Demo/SerializationDemo.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class SerializationDemo implements Serializable{
 	
-	
+	public static void main(String[] args) {
+		System.out.println("IN Serialization class ");
+	}
 	
 }
